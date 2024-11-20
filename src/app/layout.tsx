f92067027b96ea6b8c2caf0bb748deb15b7499b0
koya1616 <koya1616.0622@gmail.com>
2024-11-20 20:17:50 +0800

    ページ全体のmax-widthを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Improve stretch | 初回限定価格 | 寝屋川 | 香里園 | ストレッチ",
@@ -16,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="max-w-md mx-auto">{children}</body>
     </html>
   );
 }
